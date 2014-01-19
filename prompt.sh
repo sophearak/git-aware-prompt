953913ededba3072097c436c82a162a6c36aea5a
sophearak <t.sophearak@gmail.com>
2014-01-19 23:40:05 +0700

    add icon for branch

--- prompt.sh
+++ prompt.sh
@@ -5,7 +5,7 @@ find_git_branch() {
     if [[ "$branch" == "HEAD" ]]; then
       branch='detached*'
     fi
-    git_branch="[git::$branch]"
+    git_branch="[branch ⭠ $branch]"
   else
     git_branch=""
   fi
